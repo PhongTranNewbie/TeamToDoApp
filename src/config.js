fbce0391ecaef88a4cff5236b0e0bbfc0760e5b7
PhongTranNewbie <emphan1975@gmail.com>
2024-11-08 09:46:08 +0700

    feature-assign

---
+++ src/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    theme: 'dark', // Giao diện tối
+    itemsPerPage: 10
+};
+module.exports = {
+    allowAssignment: true, // Bật tính năng gán công việc
+    itemsPerPage: 15
+};
